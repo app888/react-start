08de2b67d38cf92805113abc448323c526c609ab
Kenneth Yeung <kenneth.sin.yeung@accenture.com>
2022-05-15 21:04:20 +0200

    remove function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Table from './Table'
 
 
 class App extends Component {
-  render() {
-
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -23,11 +22,22 @@ class App extends Component {
         job: 'Bartender',
       },
     ]
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => { return i !== index }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
 
     return (
-        
-        <div className="container">
-        <Table characterData={characters} />
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
