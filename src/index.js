dbb221806d643cae5d158d5ad40703017b2f5ee9
Kenneth Yeung <kenneth.sin.yeung@accenture.com>
2022-05-18 23:28:59 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React, { StrictMode } from 'react'
 import './index.css'
 import { createRoot } from 'react-dom/client';
 import App from './App'
+//import App from './Api';
 
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
